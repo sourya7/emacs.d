5ca74711df7457019648c77b3637927459ede772
Sourya Sharma <sourya.s7@gmail.com>
2026-09-23 10:53:00 -0600

    Adds hurl mode

--- user/work-apps.el
+++ user/work-apps.el
@@ -14,12 +14,18 @@
   :hook (elpaca-after-init . global-mise-mode))
 
 (use-package agent-shell
+  :defer t
   :custom
   (agent-shell-preferred-agent-config 'claude-code)
   (agent-shell-session-strategy 'prompt)
   (agent-shell-context-sources '(region error))
+  (agent-shell-session-restore-visibility 'full)
+  ;(agent-shell-permission-responder-function #'agent-shell-permission-allow-always)
   :config
   (setq agent-shell-anthropic-claude-environment (agent-shell-make-environment-variables :inherit-env t)))
+
+(use-package hurl-mode
+  :ensure (:host github :repo "JasZhe/hurl-mode") :defer t)
 
 (provide 'user/work-apps)
 ;;; work-apps.el ends here
